f27b4997d34a224d2fb02a57050c87104665ebf6
AntonFritsche <antonfritschedresden@gmail.com>
2025-09-25 09:18:35 +0200

    added 6 features for melting point prediction changed model.py, train.py

--- model.py
+++ model.py
@@ -2,23 +2,38 @@ import tensorflow as tf
 import tensorflow.keras.layers as layers
 
 
-def lstm_embedding(in_features: int, embedding_size: int, max_len: int) -> tf.keras.Model:
+def lstm_embedding(in_features: int, embedding_size: int) -> tf.keras.Model:
     inputs = tf.keras.layers.Input(shape=(in_features,), name="inputs")
 
-    x = tf.keras.layers.Embedding(in_features, embedding_size, input_length=max_len)(inputs)
-    x = tf.keras.layers.LSTM(64, return_sequences=True)(x)
-    x = tf.keras.layers.LSTM(64)(x)
+    x = tf.keras.layers.Embedding(in_features, embedding_size)(inputs)
+    x = tf.keras.layers.LSTM(64,
+                             return_sequences=True,
+                             activation="tanh",
+                             recurrent_activation='sigmoid'
+                             )(x)
+    x = tf.keras.layers.LSTM(64,
+                             activation="tanh",
+                             recurrent_activation='sigmoid'
+                             )(x)
+    x = tf.keras.layers.Dense(32)(x)
     x = tf.keras.layers.Dense(1)(x)
-    x = layers.LayerNormalization(epsilon=1e-6)(x)
+
 
     return tf.keras.Model(inputs=inputs, outputs=x)
 
 def lstm_stacked(in_features: int) -> tf.keras.Model:
     inputs = tf.keras.layers.Input(shape=(in_features,), name="inputs")
 
-    x = tf.keras.layers.LSTM(in_features, return_sequences=True)(inputs)
-    x = tf.keras.layers.LSTM(in_features, return_sequences=True)(x)
-    x = tf.keras.layers.LSTM(64)(x)
+    x = tf.keras.layers.LSTM(in_features,
+                             return_sequences=True,
+                             activation="tanh",
+                             recurrent_activation='sigmoid'
+                             )(inputs)
+    x = tf.keras.layers.LSTM(in_features,
+                             activation="tanh",
+                             recurrent_activation = 'sigmoid'
+                             )(x)
+    x = tf.keras.layers.LSTM(64, activation="tanh")(x)
     x = tf.keras.layers.Dense(1)(x)
     x = layers.LayerNormalization(epsilon=1e-6)(x)
 
--- train.py
+++ train.py
@@ -1,22 +1,56 @@
 from model import lstm_embedding, lstm_stacked
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+from tqdm import trange
 
 import pandas as pd
 import tensorflow as tf
 
 
-df = pd.read_csv('melting-point/train.csv')
-features = [c for c in df.columns if c.startswith("Group")]
-targetCols = "Tm"
-embedding_size = 256
+def main():
+    df = pd.read_csv('melting-point/train.csv')
+    atom_num = []
+    atom_bonds_num = []
+    tpsa = []
+    hdonors = []
+    valence_electrons = []
+    radical_electrons = []
 
-X = df[features].values
-y = df[targetCols].values
+    for i in trange(len(df)):
+        row = df["SMILES"].iloc[i]
+        mol = Chem.MolFromSmiles(row)
+        atom_num.append(mol.GetNumAtoms())
+        atom_bonds_num.append(mol.GetNumBonds())
+        descriptors = Descriptors.CalcMolDescriptors(mol)
+        tpsa.append(descriptors["TPSA"])
+        hdonors.append(descriptors["NumHDonors"])
+        valence_electrons.append(descriptors["NumValenceElectrons"])
+        radical_electrons.append(descriptors["NumRadicalElectrons"])
 
-model_embedding = lstm_embedding(len(features), embedding_size, 1)
-model_stacked = lstm_stacked(len(features))
+    df["ATOM_NUM"] = atom_num
+    df["ATOM_BONDS_NUM"] = atom_bonds_num
+    df["TPSA"] = tpsa
+    df["HDONORS"] = hdonors
+    df["VALENCE_ELECTRONS"] = valence_electrons
+    df["RADICAL_ELECTRONS"] = radical_electrons
 
-model_embedding.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-model_stacked.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    y = df["Tm"].values
+    df.drop(columns=["SMILES", "Tm", "id"], inplace=True)
+    X = df.values
 
-model_embedding.fit(X, y, epochs=10, batch_size=256, verbose=1)
-model_stacked.fit(X, y, epochs=10, batch_size=256, verbose=1)
+    print(df[:3])
+
+    embedding_size = 1024
+    model_embedding = lstm_embedding(len(df.columns), embedding_size, )
+    model_embedding.summary()
+
+    optimizer = tf.keras.optimizers.AdamW(learning_rate=0.01)
+    model_embedding.compile(optimizer=optimizer, loss=tf.keras.losses.MeanAbsoluteError, metrics=['accuracy'])
+    model_embedding.fit(X, y, epochs=25, batch_size=512, verbose=2)
+
+    #model_stacked = lstm_stacked(len(features))
+    #model_stacked.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    #model_stacked.fit(X, y, epochs=10, batch_size=256, verbose=1)
+
+if __name__ == '__main__':
+    main()
